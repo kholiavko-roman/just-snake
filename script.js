96127c5a5f1c28b1d4c8f7e7936ff94c8a5b54be
Роман Холявко <wildklas@gmail.com>
2019-12-09 00:16:53 +0200

    ADD: base food for snake. EDIT: small refactor

--- script.js
+++ script.js
@@ -1,69 +1,75 @@
-const FRAMES = 10;
+const FRAMES = 15;
 
-const canvas = document.getElementById('snakeCanvas');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("snakeCanvas");
+const context = canvas.getContext("2d");
+
 const snake = [
-  [0, 1],
-  [0, 2],
-  [0, 3],
-  [1, 3],
-  [1, 4],
+  { x: 0, y: 1 },
+  { x: 0, y: 2 },
+  { x: 0, y: 3 },
+  { x: 1, y: 3 },
+  { x: 1, y: 4 }
 ];
+let apple = { x: 9, y: 15 };
 
 context.scale(10, 10);
 
-let directions = [1, 0];
-
-document.body.addEventListener('keyup', (event) => {
-  console.log(event);
-
-  switch (event.key) {
-    case 'ArrowUp': {
-      directions = [0, -1];
-      break;
-    }
-
-    case 'ArrowRight': {
-      directions = [1, 0];
-      break;
-    }
-
-    case 'ArrowDown': {
-      directions = [0, 1];
-      break;
-    }
-
-
-    case 'ArrowLeft': {
-      directions = [-1, 0];
-      break;
-    }
-
-  }
-});
+let directions = { x: 1, y: 0 };
 
 const updateLoop = () => {
   const tail = snake.pop();
   const head = snake[0];
 
-  tail[0] = head[0] + directions[0];
-  tail[1] = head[1] + directions[1];
+  tail.x = head.x + directions.x;
+  tail.y = head.y + directions.y;
 
   snake.unshift(tail);
 
-  draw();
+  if (head.x === apple.x && head.y === apple.y) {
+    apple = { x: 0, y: 0 };
+  }
 
+  draw();
 };
 
 const draw = () => {
+  context.fillStyle = "black";
   context.clearRect(0, 0, 320, 320);
 
-  snake.forEach(([x, y]) => {
+  snake.forEach(({ x, y }) => {
     context.fillRect(x, y, 1, 1);
   });
+
+  context.fillStyle = "red";
+  context.fillRect(apple.x, apple.y, 1, 1);
 };
+
+const handleKeyup = event => {
+  switch (event.key) {
+    case "ArrowUp": {
+      directions = { x: 0, y: -1 };
+      break;
+    }
+
+    case "ArrowRight": {
+      directions = { x: 1, y: 0 };
+      break;
+    }
+
+    case "ArrowDown": {
+      directions = { x: 0, y: 1 };
+      break;
+    }
+
+    case "ArrowLeft": {
+      directions = { x: -1, y: 0 };
+      break;
+    }
+  }
+};
+
+document.body.addEventListener("keyup", handleKeyup);
 
 setInterval(() => {
   requestAnimationFrame(updateLoop);
-}, 1000 /FRAMES);
-
+}, 1000 / FRAMES);
